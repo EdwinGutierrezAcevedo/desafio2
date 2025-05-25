8fbce5a1836686fed0eb42d86dcfb1434f6142cc
EdwinGutierrezAcevedo <edwin.gutierrez1@udea.edu.co>
2025-05-25 16:41:11 -0500

    Se crearon las funciones aDias para transformar fechas a dias y getDiaSemana para obtener el dia de la semana. También se sobrecargaron los operadores - y > para operaciones con la clase fecha

--- programa/fecha.cpp
+++ programa/fecha.cpp
@@ -1,7 +1,9 @@
+#include <string>
+#include <iostream>
 #include "fecha.h"
 
-
-fecha::fecha() {}
+using namespace std;
+//fecha::fecha() {}
 bool fecha::esBisiesto(short unsigned int anio) {
     return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
 }
@@ -24,7 +26,7 @@ bool fecha::fechaValida( short unsigned int dia, short unsigned int  mes, short 
 
 }
 //setters
-void fecha::setFecha(short unsigned int d, short unsigned int  m, short unsigned int a){
+fecha::fecha(short unsigned int d, short unsigned int  m, short unsigned int a){
     if(fechaValida(d,m,a)){
         dia = d;
         mes = m;
@@ -52,10 +54,51 @@ bool fecha::operator<( fecha& other){
     if(dia>=other.getDia()) return false;
     return true;
 }
-short unsigned int fecha::operator-(fecha& other){
-    short unsigned int temporalAnio = anio-other.getAnio();
-    short unsigned int temporalMes = mes-other.getMes();
-    short unsigned int temporaldia = dia-other.getDia();
-    short unsigned int total = 365*temporalAnio+ 30* temporalMes + temporaldia;
-    return total;
+
+bool fecha::operator>( fecha& other){
+    if(anio<other.getAnio()) return false;
+    if(mes<other.getMes()) return false;
+    if(dia<=other.getDia()) return false;
+    return true;
+}
+
+ // Convierte la fecha a un número entero de días usando la fórmula del día juliano.
+unsigned int fecha::aDias() const {
+    int a = (14 - mes) / 12;
+    int y = anio + 4800 - a;
+    int m = mes + 12 * a - 3;
+    return dia + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+}
+
+unsigned int fecha::operator-(const fecha &otra) const {
+    return this->aDias() - otra.aDias();
+}
+
+
+// Método para determinar el día de la semana usando la Congruencia de Zeller.
+// Se mapea el resultado de h a un día en español:
+//   h = 0 -> Sábado, 1 -> Domingo, 2 -> Lunes, 3 -> Martes, 4 -> Miércoles, 5 -> Jueves, 6 -> Viernes.
+string fecha::getDiaSemana() const {
+    int d = dia;
+    int m = mes;
+    int y = anio;
+
+    // Si el mes es enero o febrero, se tratan como 13° y 14° mes del año anterior.
+    if (m < 3) {
+        m += 12;
+        y -= 1;
+    }
+
+    int K = y % 100;   // Año dentro del siglo
+    int J = y / 100;   // Cero basado en el siglo
+
+    int h = (d + (13 * (m + 1)) / 5 + K + (K / 4) + (J / 4) + 5 * J) % 7;
+    string dias[7] = {"Sabado", "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"};
+    return dias[h];
+}
+//pasar a apartado de despliegue
+void fecha::imprimir() const {
+    cout << (dia < 10 ? "0" : "") << dia << "/"
+              << (mes < 10 ? "0" : "") << mes << "/"
+              << anio;
 }
--- programa/fecha.h
+++ programa/fecha.h
@@ -1,20 +1,27 @@
 #ifndef FECHA_H
 #define FECHA_H
-
+#include <string>
+using namespace std;
 class fecha
 {
 private:
     short unsigned int dia,mes, anio;
 
 public:
-    fecha();
+    //fecha();
     bool fechaValida(short unsigned int dia, short unsigned int  mes, short unsigned int anio);
     bool esBisiesto(short unsigned int anio);
-    void setFecha(short unsigned int d, short unsigned int  m, short unsigned int a);
+    //void setFecha(short unsigned int d, short unsigned int  m, short unsigned int a);
+    fecha(short unsigned int d, short unsigned int  m, short unsigned int a);
     short unsigned int getDia();
     short unsigned int getMes();
     short unsigned int getAnio();
     bool operator<( fecha& other);
+    bool operator>( fecha& other);
+    unsigned int aDias() const;
+    unsigned int operator-(const fecha &otra) const;
+    string getDiaSemana() const;
+    void imprimir() const;
 
 };
 
