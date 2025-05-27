f6066912e804356b01257cb565860c6ed1145a11
Aframirez14 <a.ramirez1@udea.edu.co>
2025-05-26 19:40:02 -0500

    Implementación de las funciones: leerArchivo, CrearMatrizDeReservaciones, AgregarReservación, EliminarReservacion, ImprimirMatrizReservaciones y LiberarMatrizReservaciones

--- programa/anfitrion.cpp
+++ programa/anfitrion.cpp
@@ -2,7 +2,11 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstring>
 #include "anfitrion.h"
+
+
+
 
 anfitrion::anfitrion() {}
 
@@ -42,3 +46,105 @@ bool anfitrion::verficarDatosAnfitrion(string datos, unsigned int cedulaIngresad
     }
     return false;  // No se encontró coincidencia
 }
+
+std::string anfitrion::leerArchivoTXTreserva(const std::string &nombreArchivo) {
+    std::ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        std::cerr << "No se pudo abrir el archivo: " << nombreArchivo << std::endl;
+        return "";
+    }
+    std::ostringstream oss;
+    oss << archivo.rdbuf();
+    return oss.str();
+}
+
+char*** anfitrion::crearMatrizReservaciones(const std::string &contenido, int capacidad, int &numFilas) {
+    char*** matriz = new char**[capacidad];
+    for (int i = 0; i < capacidad; i++) {
+        matriz[i] = nullptr;
+    }
+    numFilas = 0;
+
+    std::istringstream iss(contenido);
+    std::string linea;
+    while (std::getline(iss, linea) && numFilas < capacidad) {
+        if (linea.empty()) continue;
+
+        char* cstr = new char[linea.size() + 1];
+        std::strcpy(cstr, linea.c_str());
+
+        char** fila = new char*[COLUMNAS];
+        int col = 0;
+        char* token = std::strtok(cstr, "|");
+        while (token != nullptr && col < COLUMNAS) {
+            int len = std::strlen(token) + 1;
+            fila[col] = new char[len];
+            std::strcpy(fila[col], token);
+            col++;
+            token = std::strtok(nullptr, "|");
+        }
+        while (col < COLUMNAS) {
+            fila[col] = new char[1];
+            fila[col][0] = '\0';
+            col++;
+        }
+        matriz[numFilas] = fila;
+        numFilas++;
+        delete[] cstr;
+    }
+    return matriz;
+}
+
+void anfitrion::agregarReservacion(char*** &matriz, int &numFilas, int capacidad, const char *datos[COLUMNAS]) {
+    if (numFilas == capacidad) {
+        std::cout << "Capacidad alcanzada (" << capacidad << "). No se puede agregar más reservaciones." << std::endl;
+        return;
+    }
+    char** fila = new char*[COLUMNAS];
+    for (int j = 0; j < COLUMNAS; j++) {
+        int len = std::strlen(datos[j]) + 1;
+        fila[j] = new char[len];
+        std::strcpy(fila[j], datos[j]);
+    }
+    matriz[numFilas] = fila;
+    numFilas++;
+}
+
+void anfitrion::eliminarReservacion(char*** &matriz, int &numFilas, int indice) {
+    if (indice < 0 || indice >= numFilas) {
+        std::cout << "Índice fuera de rango. No se puede eliminar." << std::endl;
+        return;
+    }
+    for (int j = 0; j < COLUMNAS; j++) {
+        delete[] matriz[indice][j];
+    }
+    delete[] matriz[indice];
+
+    for (int i = indice; i < numFilas - 1; i++) {
+        matriz[i] = matriz[i + 1];
+    }
+    matriz[numFilas - 1] = nullptr;
+    numFilas--;
+}
+
+void anfitrion::imprimirMatrizReservaciones(char*** matriz, int numFilas) {
+    std::cout << "\nMatriz de Reservaciones:" << std::endl;
+    for (int i = 0; i < numFilas; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            std::cout << std::left << std::setw(15) << matriz[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+void anfitrion::liberarMatrizReservaciones(char*** &matriz, int numFilas) {
+    for (int i = 0; i < numFilas; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            delete[] matriz[i][j];
+        }
+        delete[] matriz[i];
+    }
+    delete[] matriz;
+    matriz = nullptr;
+}
+
--- programa/anfitrion.h
+++ programa/anfitrion.h
@@ -1,6 +1,9 @@
 #ifndef ANFITRION_H
 #define ANFITRION_H
 #include <string>
+#include <iomanip>
+#include <sstream>
+#define COLUMNAS 6
 using namespace std;
 class anfitrion
 {
@@ -8,10 +11,17 @@ class anfitrion
     string contrasenaAnfitrion;
     friend string leerArchivoTXT(const string& nombreArchivo);
     bool verficarDatosAnfitrion(string datos, unsigned int cedulaIngresada, string contrasenaIngresada);
+
 public:
     void setDocumentoAnfitrion(const unsigned int &doc);
     void setContrasenaAnfitrion(const string &contra);
     anfitrion();
+    static string leerArchivoTXTreserva(const string &nombreArchivo);
+    static char*** crearMatrizReservaciones(const string &contenido, int capacidad, int &numFilas);
+    static void agregarReservacion(char*** &matriz, int &numFilas, int capacidad, const char *datos[COLUMNAS]);
+    static void eliminarReservacion(char*** &matriz, int &numFilas, int indice);
+    static void imprimirMatrizReservaciones(char*** matriz, int numFilas);
+    static void liberarMatrizReservaciones(char*** &matriz, int numFilas);
 };
 
 #endif // ANFITRION_H
